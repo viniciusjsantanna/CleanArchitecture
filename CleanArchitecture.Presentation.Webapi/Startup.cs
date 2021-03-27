07c3868f4bc049c49c5302303efe75ea7124d0bf
Vinicius Santana <vinicius.santana@localiza.com>
2021-03-27 20:33:27 -0300

    Adding cors configuration

--- CleanArchitecture.Presentation.Webapi/Startup.cs
+++ CleanArchitecture.Presentation.Webapi/Startup.cs
@@ -2,17 +2,10 @@ using CleanArchitecture.Application.IoC;
 using CleanArchitecture.Infrastructure.IoC;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace CleanArchitecture.Presentation.Webapi
 {
@@ -31,6 +24,10 @@ namespace CleanArchitecture.Presentation.Webapi
             services.AppRegister();
             services.InfraRegister(Configuration);
             services.AddControllers();
+            services.AddCors(config =>
+            {
+                config.AddPolicy("host", opt => opt.WithOrigins("http://localhost:4500").AllowAnyMethod().AllowAnyHeader());
+            });
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "CleanArchitecture.Presentation.Webapi", Version = "v1" });
@@ -46,6 +43,8 @@ namespace CleanArchitecture.Presentation.Webapi
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "CleanArchitecture.Presentation.Webapi v1"));
             }
+
+            app.UseCors("host");
 
             app.UseHttpsRedirection();
 
